68c95d867460a45eb117b23149faf60d63621627
Jon Pentland <jon@iomedia.co.uk>
2015-06-24 12:24:59 +0100

    Generate pdfs from forms. Create email_form_as_pdf() function.

--- pretaweb/plomino2pdf/api.py
+++ pretaweb/plomino2pdf/api.py
@@ -1,3 +1,6 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 from lxml import etree
 from plone import api
 from plone.memoize import view
@@ -11,14 +14,75 @@ from xhtml2pdf.document import pisaDocument
 from zope.component import getAdapters
 from zope.contenttype import guess_content_type, text_type
 import urlparse
+import logging
+logger=logging.getLogger('preteweb.plomino2pdf.api')
 
 def sort_key(a, b):
     return cmp(a.order, b.order)
 
-def generate_pdf(content):
-    pdf = StringIO()
+def email_form_as_pdf(form,recipient,subject=None,content=None):
+    """ Builds a PDF of the submitted form and emails it to the recipient """
+    # Get the html with values
+    portal = api.portal.get()
+    request = getattr(form,"REQUEST",None)
+
+    # Use the custom view DisplayForm to render the form from the request
+    displayform = subrequest('./DisplayForm')
+
+    # Transform the html
+    html=transform_html(displayform.getBody(),request)
+
+    # Generate the pdf
+    pdf = generate_pdf(html,form.absolute_url(),request)
+    pdf_name=form.getId()
+
+    # Create an email
+    message = MIMEMultipart()
+    if subject is None:
+        subject='%s PDF Attached' % form.title
+    message['Subject'] = subject
+    message['From'] = '%s <%s>' % (portal.getProperty('email_from_name'),
+                                   portal.getProperty('email_from_address'))
+    message['To'] = recipient
+    if content is None:
+        content = 'Attached PDF %s.pdf' % pdf_name
+    message_content = MIMEText('%s\n' % content)
+    message.attach(message_content)
+
+    # Attach the pdf
+    pdf_mime = MIMEApplication(pdf)
+    pdf_mime.add_header(
+        'Content-Disposition','attachment',
+        filename='%s.pdf' % pdf_name
+    )
+    message.attach(pdf_mime)
+
+    # Send the email
+    send_mime_message(message)
+
+def send_mime_message(message):
+    mh = api.portal.get_tool('MailHost')
+    return mh.send(message, immediate=True)
+
+def transform_html(html,request):
     portal = api.portal.get()
     charset = portal.portal_properties.site_properties.default_charset
+    new_html = None
+    published = request.get('PUBLISHED', None)
+    handlers = [v[1] for v in getAdapters((published, request,), ITransform)]
+    handlers.sort(sort_key)
+    if handlers:
+        # The first handler is the diazo transform, the other 4 handlers are caching
+        theme_handler = handlers[0]
+        new_html = theme_handler.transformIterable([html], charset)
+    # If the theme is not enabled, transform returns None
+    if new_html is not None:
+        new_html = etree.tostring(new_html.tree)
+    else:
+        new_html = html
+    return new_html
+
+def generate_pdf(html,path,request):
 
     def fetch_resources(uri, rel):
         """
@@ -67,34 +131,13 @@ def generate_pdf(content):
         data_uri = 'data:{0};base64,{1}'.format(ctype, data)
         return data_uri
 
-    meta_type = content.meta_type
-    if meta_type == 'PlominoView':
-        html = content.checkBeforeOpenView()
-    elif meta_type == 'PlominoDocument':
-        html = content.checkBeforeOpenDocument()
-    elif meta_type == 'PlominoForm':
-        html = content.OpenForm(searchresults=[])
-    else:
-        html = getattr(content, content.default_view)()
-    new_html = None
-    request = getattr(content,"REQUEST",None)
-    published = request.get('PUBLISHED', None)
-    handlers = [v[1] for v in getAdapters((published, request,), ITransform)]
-    handlers.sort(sort_key)
-    if handlers:
-        # The first handler is the diazo transform, the other 4 handlers are caching
-        theme_handler = handlers[0]
-        new_html = theme_handler.transformIterable([html], charset)
-    # If the theme is not enabled, transform returns None
-    if new_html is not None:
-        new_html = etree.tostring(new_html.tree)
-    else:
-        new_html = html
-    pisadoc = pisaDocument(new_html, pdf, path=content.absolute_url(),
+    pdf = StringIO()
+
+    pisadoc = pisaDocument(html,pdf, path=path,
                            raise_exception=True, link_callback=fetch_resources)
     # pisadoc = pisaDocument(html, pdf, raise_exception=True)
     assert pdf.len != 0, 'Pisa PDF generation returned empty PDF!'
-    #html.close()
-    pdfcontent = pdf.getvalue()
+
+    pdf_content = pdf.getvalue()
     pdf.close()
-    return pdfcontent
+    return  pdf_content
